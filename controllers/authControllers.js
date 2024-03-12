baccb81bb9e6ef50ad22b5561f72318b9317d6d0
Chintan <patelchintan18018@gmail.com>
2024-03-12 20:40:53 +0530

    new structure

---
+++ controllers/authControllers.js
@@ -0,0 +1,67 @@
+// controllers/authController.js
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.registerUser = async (req, res) => {
+  try {
+    let check = await User.findOne({ email: req.body.email });
+    if (check) {
+      return res.json({
+        success: false,
+        message: "Existing user found with the same Email ID",
+      });
+    }
+    const user = new User({
+      name: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await user.save();
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const token = jwt.sign(data, "secret-ecom");
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "Wrong Email",
+      });
+    }
+    let passwordcheck = req.body.password === user.password;
+    if (passwordcheck) {
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, "secret-ecom");
+      res.json({
+        success: true,
+        token,
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "Wrong password",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
